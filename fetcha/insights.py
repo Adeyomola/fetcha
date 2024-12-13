6c55064dd3871df19034f8c947c2c719dd059b3b
Adeyomola <kayzeel@yahoo.com>
2024-12-13 02:19:38 +0100

    -

--- fetcha/insights.py
+++ fetcha/insights.py
@@ -34,7 +34,8 @@ def insights(identifier='butterskinned'):
     select_counts = text(f"SELECT {countries_string} FROM insights WHERE identifier='{identifier}'")
     counts = connection.execute(select_counts).fetchall()[0]
     counts = list(counts)
-    counts = map(int, counts)
+    counts = counts.replace(",", "")
+    counts = counts.replace("'", "")
 
 
     return render_template('insights.html', identifier=identifier, counts=counts, countries=countries_string) #available_days = available_days[2]
