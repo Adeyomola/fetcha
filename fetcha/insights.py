f83bcee3092f735ef64e2c9605bd24fa1755da8a
Adeyomola <kayzeel@yahoo.com>
2024-12-14 01:38:12 +0100

    added full form of countries abbrv

--- fetcha/insights.py
+++ fetcha/insights.py
@@ -4,7 +4,11 @@ from .db import get_db
 from sqlalchemy import text
 from werkzeug.exceptions import abort
 from .auth import login_required
+from urllib.request import urlopen
+import json
 
+iso3166 = urlopen('https://gist.githubusercontent.com/ssskip/5a94bfcd2835bf1dea52/raw/59272a2d1c2122f0cedd83a76780a01d50726d98/ISO3166-1.alpha2.json')
+iso3166 = json.loads(iso3166.read())
 
 bp = Blueprint('insights', __name__)
 md = metadata()
@@ -22,6 +26,8 @@ def insights(identifier):
     countries = countries.replace(",", "")
     countries = countries.split()
     countries.remove('identifier')
+
+    countries = [abbreviation.replace(abbreviation, iso3166[abbreviation]) for abbreviation in countries]
 
     countries_string = ', '.join(countries)
 
